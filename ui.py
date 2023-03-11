85794e5da7498535ff56a76de40319129f82c649
qpal147147 <43807991+qpal147147@users.noreply.github.com>
2023-03-11 14:51:25 +0800

    fix default source

--- ui.py
+++ ui.py
@@ -21,7 +21,7 @@ def detect(img, save_img=False):
     
     parser = argparse.ArgumentParser()
     parser.add_argument('--weights', nargs='+', type=str, default=model, help='model.pt path(s)')
-    parser.add_argument('--source', type=str, default='inference/', help='source')  # file/folder, 0 for webcam
+    parser.add_argument('--source', type=str, default='example/', help='source')  # file/folder, 0 for webcam
     parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
     parser.add_argument('--conf-thres', type=float, default=0.25, help='object confidence threshold')
     parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
